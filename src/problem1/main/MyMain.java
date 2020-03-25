79820d0a935495f315a289690758aaab919d4cb3
Mayanksingh2004 <mayank.singh1_cs18@gla.ac.in>
2020-03-25 15:31:53 +0530

    MyBinarySearchTree class - Created getter for the root node.

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -6,7 +6,20 @@
  */
 package problem1.main;
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
     public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        tree.insert(50);
+        tree.insert(50);
+        tree.insert(50);
+        tree.insert(50);
+        tree.insert(50);
+        tree.insert(50);
+        tree.insert(50);
+        tree.insert(50);
+        tree.insert(50);
+
     }
 }
--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -13,6 +13,10 @@ public class MyBinarySearchTree {
 
     private TreeNode root = null;
 
+    public TreeNode getRoot() {
+        return root;
+    }
+
     public void insert(int data) {
         TreeNode node = new TreeNode(data);
         if (root == null) {
