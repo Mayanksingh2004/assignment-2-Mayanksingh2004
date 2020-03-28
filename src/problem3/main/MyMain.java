6175dbbb2a7ecf6c1f80b51eca35c8fbe2c5c8c2
Mayanksingh2004 <mayank.singh1_cs18@gla.ac.in>
2020-03-28 13:36:23 +0530

    Problem3 - MyPriorityQueue - completed insert() method

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -6,10 +6,19 @@
  */
 package problem3.main;
 
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
     public static void main(String[] args) {
-
+        MyPriorityQueue queue = new MyPriorityQueue();
+        queue.push(181500372, "Loki");
+        queue.push(181500348, "Mayank");
+        queue.push(181500358, "Halo");
+        queue.push(181500399, "Zeus");
+        queue.push(181500009, "Atreus");
+        queue.push(181500010, "Kratos");
+        queue.print();
     }
 }
--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -9,29 +9,31 @@ package problem3.myqueue;
 import problem3.node.Node;
 
 public class MyPriorityQueue {
-    private Node head = null;
+    private Node front = null;
 
     public void push(int rollNumber, String name) {
         Node node = new Node(rollNumber, name);
-        if (head == null) {
-            head = node;
+        if (front == null) {
+            front = node;
         } else {
-            if (head.getRollNumber() > node.getRollNumber()) {
-                node.setNext(head);
-                head = node;
+            if (front.getRollNumber() > node.getRollNumber()) {
+                node.setNext(front);
+                front = node;
             } else {
-                Node temp = head;
+                Node temp = front;
                 Node previous = null;
-                while (temp != null) {
+                while (temp != null && temp.getRollNumber() < node.getRollNumber()) {
                     previous = temp;
                     if (temp.getRollNumber() < node.getRollNumber()) {
                         temp = temp.getNext();
-                    } else {
-                        previous.setNext(node);
-                        node.setNext(temp);
                     }
+                }
+                previous.setNext(node);
+                if (temp != null) {
+                    node.setNext(temp);
                 }
             }
         }
     }
+
 }
