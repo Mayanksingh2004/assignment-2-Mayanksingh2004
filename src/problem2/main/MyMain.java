972f7d9476d21a1061c9d1d73bd052608df87afa
Mayanksingh2004 <mayank.singh1_cs18@gla.ac.in>
2020-03-28 18:31:56 +0530

    Problem2 - improved the code.

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -6,11 +6,35 @@
  */
 package problem2.main;
 
+import problem1.mybst.MyBinarySearchTree;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
 
     public static void main(String[] args) {
-
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        tree.insert(50);
+        tree.insert(25);
+        tree.insert(75);
+        tree.insert(15);
+        tree.insert(35);
+        tree.insert(65);
+        tree.insert(80);
+        tree.insert(7);
+        tree.insert(32);
+        tree.insert(31);
+        tree.insert(33);
+        tree.insert(67);
+        tree.insert(90);
+        tree.insert(82);
+        tree.insert(93);
+        System.out.println("PreOrder : ");
+        // here root is the first element
+        tree.traversePreOrder(tree.getRoot());
+        System.out.println("\nPostOrder :");
+        //here root is the last element
+        tree.traversePostOrder(tree.getRoot());
+        //they hava a common midpoint
     }
 }
